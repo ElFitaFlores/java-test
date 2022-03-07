d8b396e9efaa414f346205481a66d37556d84c81
Rafael Flores <fita500@gmail.com>
2022-03-06 19:00:32 -0600

    Add multiple tests to string util

--- src/main/java/com/example/javatest/util/StringUtil.java
+++ src/main/java/com/example/javatest/util/StringUtil.java
@@ -2,6 +2,10 @@ package com.example.javatest.util;
 
 public class StringUtil {
     public static String repeat(String str, Integer times) {
+        if (times < 0) {
+            throw new IllegalArgumentException("Negatives not allowed.");
+        }
+
         String result = "";
 
         for (int i = 0; i < times; i++) {
--- src/test/java/com/example/javatest/util/StringUtilTest.java
+++ src/test/java/com/example/javatest/util/StringUtilTest.java
@@ -1,9 +1,34 @@
 package com.example.javatest.util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringUtilTest {
-    public static void main(String[] args) {
+    @Test
+    public void repeat_string_once() {
+        String result = StringUtil.repeat("hola", 1);
+
+        Assert.assertEquals("hola", result);
+    }
+
+    @Test
+    public void repeat_string_multiple_times() {
         String result = StringUtil.repeat("hola", 3);
 
-        System.out.println(result);
+        Assert.assertEquals("holaholahola", result);
+    }
+
+    @Test
+    public void repeat_string_zero_times() {
+        String result = StringUtil.repeat("hola", 0);
+
+        Assert.assertEquals("", result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times() {
+        String result = StringUtil.repeat("hola", -1);
+
+        Assert.assertEquals("", result);
     }
 }
