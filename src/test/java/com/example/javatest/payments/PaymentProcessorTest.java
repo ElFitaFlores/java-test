f0783e1a1e77afabee4432cc6f6627dff846a3f1
Rafael Flores <fita500@gmail.com>
2022-03-08 17:40:36 -0600

    Improve PaymentProcessorTest

--- src/test/java/com/example/javatest/payments/PaymentProcessorTest.java
+++ src/test/java/com/example/javatest/payments/PaymentProcessorTest.java
@@ -1,29 +1,33 @@
 package com.example.javatest.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
+    PaymentGateway paymentGateway;
+    PaymentProcessor paymentProcessor;
+
+    @Before
+    public void setup() {
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
     @Test
     public void payment_is_correct() {
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.paymentRequest(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         assertTrue(paymentProcessor.makePayment(1000));
     }
 
     @Test
     public void payment_is_wrong() {
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.paymentRequest(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         assertFalse(paymentProcessor.makePayment(1000));
     }
